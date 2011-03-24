f9185af3df751fbe568b73f9c2435b800429f5bd
Udo Schroeter <udo.schroeter@gmail.com>
2011-03-24 04:12:32 +0000

    basic profile pages

--- mvc/home/home.index.php
+++ mvc/home/home.index.php
@@ -4,6 +4,6 @@ include_once('templates/postlist.php');
 
 include('templates/publish.thingy.php');
 
-tmpl_postlist($this->msg->getStream($this->user->ds['u_key'], $this->user->entity), true);
+tmpl_postlist($this->msg->getStream($this->user->entity), true);
 
 ?>
